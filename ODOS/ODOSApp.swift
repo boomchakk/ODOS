3bd9524c8df9a8061f9019d40c8b4b67221c5338
boomchakk <103128249+boomchakk@users.noreply.github.com>
2025-04-20 14:13:14 +1000

    Initial Commit

---
+++ ODOS/ODOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ODOSApp.swift
+//  ODOS
+//
+//  Created by Brent Chaker on 20/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct ODOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
